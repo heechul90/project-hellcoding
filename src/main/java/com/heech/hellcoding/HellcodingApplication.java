4e125d273e73b00e694629eb87f9a08db753ded9
hclee <heechul4296@gmail.com>
2022-03-25 22:09:23 +0900

    add source

---
+++ src/main/java/com/heech/hellcoding/HellcodingApplication.java
@@ -0,0 +1,13 @@
+package com.heech.hellcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellcodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellcodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heech/hellcoding/HellcodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heech.hellcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellcodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
