85d1a5e70dc963bcd1942897be7848666694e264
hclee <heechul4296@gmail.com>
2022-04-02 23:09:10 +0900

    Item form 생성

---
+++ src/main/java/com/heech/hellcoding/item/web/form/ItemUpdateForm.java
@@ -0,0 +1,26 @@
+package com.heech.hellcoding.item.web.form;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class ItemUpdateForm {
+
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max=1000000)
+    private Integer price;
+
+    private Integer quantity;
+}
