2c34e11182b7c543c061ce1c7842a94a98cbd04a
hclee <heechul4296@gmail.com>
2022-04-03 17:58:10 +0900

    상품 등록 추가

--- src/main/java/com/heech/hellcoding/item/web/AdminItemController.java
+++ src/main/java/com/heech/hellcoding/item/web/AdminItemController.java
@@ -2,14 +2,17 @@ package com.heech.hellcoding.item.web;
 
 import com.heech.hellcoding.item.domain.Item;
 import com.heech.hellcoding.item.service.ItemService;
+import com.heech.hellcoding.item.web.form.ItemSaveForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,18 +24,67 @@ public class AdminItemController {
 
     private final ItemService itemService;
 
+    /**
+     * 상품 목록
+     * @param model
+     * @return
+     */
     @GetMapping(value = "/list")
-    public String item(Model model) {
+    public String itemList(Model model) {
         List<Item> itemList = itemService.findAll();
         model.addAttribute("itemList", itemList);
         return "admin/item/itemList";
     }
 
+    /**
+     * 상품 상세
+     * @param itemId
+     * @param model
+     * @return
+     */
     @GetMapping(value = "/list/{itemId}")
-    public String item(@PathVariable long itemId, Model model) {
+    public String itemDetail(@PathVariable long itemId, Model model) {
         Optional<Item> item = itemService.findById(itemId);
-        model.addAttribute("item", item);
+        model.addAttribute("item", item.get());
         return "admin/item/itemDetail";
+    }
+
+    @GetMapping(value = "/add")
+    public String addItemForm(Model model) {
+        model.addAttribute("item", new ItemSaveForm());
+        return "admin/item/addItemForm";
+    }
+
+    @PostMapping(value = "/add")
+    public String addItem(@Validated @ModelAttribute("item") ItemSaveForm form, BindingResult bindingResult,
+                          RedirectAttributes redirectAttributes) {
+        //특정 필드 예외가 아닌 전체 예외
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getPrice() * form.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult={}", bindingResult);
+            return "admin/item/addItemForm";
+        }
+
+        Item savedItem = itemService.save(new Item(form.getItemName(), form.getPrice(), form.getQuantity()));
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/admin/item/list/{itemId}";
+    }
+
+    @GetMapping(value = "/{itemId}/edit")
+    public String editItemForm(Model model) {
+        return "admin/item/editItemForm";
+    }
+
+    @PostMapping(value = "/{itemId}/edit")
+    public String editItem(Model model) {
+        return "";
     }
 
 }
