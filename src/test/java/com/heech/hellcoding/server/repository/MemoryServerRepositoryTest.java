b730d97b9fbdab5068c303f56121174b8df70c59
hclee <hclee@dataeum.kr>
2022-04-13 22:05:52 +0900

    서버관리 테스트 추가중..

--- src/test/java/com/heech/hellcoding/server/repository/MemoryServerRepositoryTest.java
+++ src/test/java/com/heech/hellcoding/server/repository/MemoryServerRepositoryTest.java
@@ -1,8 +1,11 @@
 package com.heech.hellcoding.server.repository;
 
+import com.heech.hellcoding.server.domain.Server;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemoryServerRepositoryTest {
@@ -16,6 +19,16 @@ class MemoryServerRepositoryTest {
 
     @Test
     void save() {
+        //given
+        Server server = new Server("serverA", "127.0.0.1", "ubuntu", "intel", "nvidia");
+
+        //when
+        Server savedServer = serverRepository.save(server);
+
+        //then
+        Server findServer = serverRepository.findById(server.getId()).get();
+        assertThat(findServer).isEqualTo(savedServer);
+
     }
 
     @Test
