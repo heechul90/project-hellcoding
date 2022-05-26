933bf3c850333ac629a381fbad11f4d84875b63d
hclee <hclee@dataeum.kr>
2022-05-26 20:12:48 +0900

    orderRepository findByIdTest 추가

--- src/test/java/com/heech/hellcoding/core/shop/order/repository/OrderRepositoryTest.java
+++ src/test/java/com/heech/hellcoding/core/shop/order/repository/OrderRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.heech.hellcoding.core.shop.order.repository;
 
+import com.heech.hellcoding.core.common.entity.Address;
+import com.heech.hellcoding.core.common.exception.NoSuchElementException;
 import com.heech.hellcoding.core.member.domain.Member;
 import com.heech.hellcoding.core.shop.delivery.domain.Delivery;
+import com.heech.hellcoding.core.shop.delivery.domain.DeliveryStatus;
 import com.heech.hellcoding.core.shop.order.domain.Order;
 import com.heech.hellcoding.core.shop.order.domain.OrderStatus;
 import org.junit.jupiter.api.Test;
@@ -21,7 +24,7 @@ class OrderRepositoryTest {
     OrderRepository orderRepository;
 
     @Test
-    void createOrder() {
+    void saveOrderTest() throws Exception {
         //given
         Member member = new Member("loginId", "memberA", "1111");
         Order order = new Order(LocalDateTime.now(), OrderStatus.ORDER, member, new Delivery());
@@ -30,9 +33,28 @@ class OrderRepositoryTest {
         Order savedOrder = orderRepository.save(order);
 
         //then
-        Order findOrder = orderRepository.findById(order.getId()).orElse(null);
+        Order findOrder = orderRepository.findById(order.getId()).orElseThrow(() -> new NoSuchElementException("조회에 실패했습니다."));
         assertThat(findOrder).isEqualTo(order);
         assertThat(findOrder.getStatus()).isEqualTo(OrderStatus.ORDER);
+    }
+
+    @Test
+    void findByIdTest() {
+        //given
+        Member member = new Member("loginId", "memberA", "1111");
+        Address address = new Address("12345", "Seoul", "gangnamdaero");
+        Delivery delivery = new Delivery(address, DeliveryStatus.READY);
+        Order order = new Order(LocalDateTime.now(), OrderStatus.ORDER, member, delivery);
+        Order savedOrder = orderRepository.save(order);
+
+        //when
+        Order findOrder = orderRepository.findById(order.getId()).orElseThrow(() -> new NoSuchElementException("조회에 실패했습니다."));
+
+        //then
+        assertThat(findOrder).isEqualTo(order);
+        assertThat(findOrder.getMember()).isEqualTo(member);
+        assertThat(findOrder.getDelivery().getAddress()).isEqualTo(address);
+        assertThat(findOrder.getDelivery().getStatus()).isEqualTo(DeliveryStatus.READY);
     }
 
 }
