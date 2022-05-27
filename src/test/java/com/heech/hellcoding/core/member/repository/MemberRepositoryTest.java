96df40d7a58d43d64669ae496065529eca944197
hclee <hclee@dataeum.kr>
2022-05-27 17:50:40 +0900

    MemberRepositoryTest 정리

--- src/test/java/com/heech/hellcoding/core/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/heech/hellcoding/core/member/repository/MemberRepositoryTest.java
@@ -7,18 +7,15 @@ import com.heech.hellcoding.core.member.domain.Mobile;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-//@Rollback(value = false)
 class MemberRepositoryTest {
 
     @Autowired
