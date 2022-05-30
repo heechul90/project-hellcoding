1bc11dae3dcc85259001527b5e16fa707253381e
hclee <heechul4296@gmail.com>
2022-05-31 00:12:39 +0900

    item > book > BookRepositoryTest > findBooksTest 추가

--- src/test/java/com/heech/hellcoding/core/shop/item/book/repository/BookRepositoryTest.java
+++ src/test/java/com/heech/hellcoding/core/shop/item/book/repository/BookRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.heech.hellcoding.core.shop.item.book.repository;
 
+import com.heech.hellcoding.core.common.dto.SearchCondition;
 import com.heech.hellcoding.core.shop.item.book.domain.Book;
 import com.heech.hellcoding.core.shop.item.book.dto.BookSearchCondition;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -25,17 +30,38 @@ class BookRepositoryTest {
     @Test
     public void findBooksTest() throws Exception{
         //given
+        for (int i = 0; i <50; i++) {
+            bookRepository.save(
+                    Book.builder()
+                            .name("상품이름" + i)
+                            .title("상품타이틀" + i)
+                            .content("상품내용" + i)
+                            .price(7200 + i)
+                            .stockQuantity(200 + i)
+                            .author("저자" + i)
+                            .isbn(UUID.randomUUID().toString().toUpperCase())
+                            .build()
+            );
+        }
 
         //when
         BookSearchCondition condition = new BookSearchCondition();
         PageRequest pageRequest = PageRequest.of(0, 10);
-        Page<Book> content = bookRepository.findBooks(condition, pageRequest);
-
-        for (Book book : content.getContent()) {
-            System.out.println("book.getName() = " + book.getName());
-            System.out.println("book.getAuthor() = " + book.getAuthor());
-        }
+        Page<Book> content1 = bookRepository.findBooks(condition, pageRequest);
 
         //then
+        assertThat(content1.getContent().size()).isEqualTo(10);
+
+        //when
+        condition.setSearchCondition(SearchCondition.NAME);
+        condition.setSearchKeyword("이름");
+        condition.setSearchPriceGoe(7245);
+        Page<Book> content2 = bookRepository.findBooks(condition, pageRequest);
+
+        //then
+        assertThat(content2.getContent().size()).isEqualTo(5);
+        assertThat(content2.getContent())
+                .extracting("name")
+                .containsExactly("상품이름45", "상품이름46", "상품이름47", "상품이름48", "상품이름49");
     }
 }
