d79eb68cc43209e0eca5581f7f34d5094ada91b9
hclee <heechul4296@gmail.com>
2022-06-25 20:05:06 +0900

    pay > bank > entity 생성

--- src/main/java/com/heech/hellcoding/core/pay/bank/domain/Bank.java
+++ src/main/java/com/heech/hellcoding/core/pay/bank/domain/Bank.java
@@ -1,4 +1,17 @@
 package com.heech.hellcoding.core.pay.bank.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Bank {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bank_id")
+    private Long id;
 }
