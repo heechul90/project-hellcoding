cf25ca7a29502656d35c547aa4f8c31c71f69bed
hclee <hclee@dataeum.kr>
2022-07-01 15:14:40 +0900

    survey > questionnaire > option service > test 추가

--- src/test/java/com/heech/hellcoding/core/survey/option/service/OptionServiceTest.java
+++ src/test/java/com/heech/hellcoding/core/survey/option/service/OptionServiceTest.java
@@ -2,9 +2,10 @@ package com.heech.hellcoding.core.survey.option.service;
 
 import com.heech.hellcoding.core.common.exception.NoSuchElementException;
 import com.heech.hellcoding.core.survey.option.domain.Option;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +13,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -24,13 +24,34 @@ class OptionServiceTest {
     @Autowired
     OptionService optionService;
 
-    @Test
-    void deleteOptionTest() {
-        //given
+    private Option getOption() {
         Option option = Option.createOptionBuilder()
                 .optionOrder(1)
                 .content("test_option_content")
                 .build();
+        return option;
+    }
+
+    @Test
+    public void findOptionTest() throws Exception{
+        //given
+        Option option = getOption();
+        em.persist(option);
+        em.flush();
+        em.clear();
+
+        //when
+        Option findOption = optionService.findOption(option.getId());
+
+        //then
+        assertThat(findOption.getOptionOrder()).isEqualTo(1);
+        assertThat(findOption.getContent()).isEqualTo("test_option_content");
+    }
+
+    @Test
+    void deleteOptionTest() {
+        //given
+        Option option = getOption();
         em.persist(option);
         em.flush();
         em.clear();
