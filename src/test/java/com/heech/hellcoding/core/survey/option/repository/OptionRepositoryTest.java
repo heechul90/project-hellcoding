55dc0c4656d7894d255525d0c5cd1761c21f3861
hclee <hclee@dataeum.kr>
2022-07-01 14:51:48 +0900

    survey > questionnaire > option repository > test 추가

--- src/test/java/com/heech/hellcoding/core/survey/option/repository/OptionRepositoryTest.java
+++ src/test/java/com/heech/hellcoding/core/survey/option/repository/OptionRepositoryTest.java
@@ -1,27 +1,44 @@
 package com.heech.hellcoding.core.survey.option.repository;
 
+import com.heech.hellcoding.core.survey.option.domain.Option;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@SpringBootTest
-@Transactional
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class OptionRepositoryTest {
 
     @PersistenceContext
     EntityManager em;
 
+    @Autowired
+    OptionRepository optionRepository;
+
     @Test
     public void saveOptionTest() throws Exception{
         //given
-        System.out.println("test start!");
+        Option option = Option.createOptionBuilder()
+                .optionOrder(1)
+                .content("test_content")
+                .build();
 
         //when
+        Option savedOption = optionRepository.save(option);
+        em.flush();
+        em.clear();
 
         //then
+        Option findOption = optionRepository.findById(savedOption.getId()).orElse(null);
+        assertThat(findOption.getOptionOrder()).isEqualTo(1);
+        assertThat(findOption.getContent()).isEqualTo("test_content");
     }
 
 }
