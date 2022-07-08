2657e94a57c713eec2fcd56ec44a7488dfb9a192
hclee <hclee@dataeum.kr>
2022-07-08 10:15:19 +0900

    education > lesson > entity 수정

--- src/main/java/com/heech/hellcoding/core/education/lesson/domain/LessonType.java
+++ src/main/java/com/heech/hellcoding/core/education/lesson/domain/LessonType.java
@@ -1,5 +1,13 @@
 package com.heech.hellcoding.core.education.lesson.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum LessonType {
-    CONTENT, PLAN
+
+    CONTENT("콘텐츠"), PLAN("지도안");
+
+    private final String code;
 }
