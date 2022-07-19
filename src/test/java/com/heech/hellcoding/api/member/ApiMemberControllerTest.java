6efea14dc2dca17e5da67a3f492a48cc59bd2de8
hclee <hclee@dataeum.kr>
2022-07-19 21:32:59 +0900

    member > controller > test 추가중...

--- src/test/java/com/heech/hellcoding/api/member/ApiMemberControllerTest.java
+++ src/test/java/com/heech/hellcoding/api/member/ApiMemberControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,15 +16,12 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import static org.assertj.core.api.BDDAssumptions.given;
-import static org.junit.jupiter.api.Assertions.*;
-
 @WebMvcTest(ApiMemberController.class)
 @AutoConfigureMockMvc
 @Transactional
 class ApiMemberControllerTest {
 
-    //@PersistenceContext private EntityManager em;
+    @PersistenceContext private EntityManager em;
 
     @Autowired private MockMvc mockMvc;
 
@@ -38,7 +34,15 @@ class ApiMemberControllerTest {
     @Test
     void findMemberTest() throws Exception {
         //given
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/members/{id}", 1L)
+        Member member = Member.createMemberBuilder()
+                .name("test")
+                .loginId("test")
+                .password("test")
+                .email("test")
+                .build();
+        em.persist(member);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/members/{id}", member.getId())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print());
     }
