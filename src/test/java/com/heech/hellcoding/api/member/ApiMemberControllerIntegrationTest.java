179604bc58c85e60a4a056868f3c293a07699086
hclee <hclee@dataeum.kr>
2022-07-30 21:11:29 +0900

    member > controller 통합테스트 findMember 추가

--- src/test/java/com/heech/hellcoding/api/member/ApiMemberControllerIntegrationTest.java
+++ src/test/java/com/heech/hellcoding/api/member/ApiMemberControllerIntegrationTest.java
@@ -1,14 +1,48 @@
 package com.heech.hellcoding.api.member;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.heech.hellcoding.api.member.request.CreateMemberRequest;
+import com.heech.hellcoding.api.member.request.UpdateMemberRequest;
+import com.heech.hellcoding.core.common.dto.SearchCondition;
 import com.heech.hellcoding.core.common.entity.Address;
 import com.heech.hellcoding.core.member.domain.AuthorCode;
 import com.heech.hellcoding.core.member.domain.GenderCode;
+import com.heech.hellcoding.core.member.domain.Member;
 import com.heech.hellcoding.core.member.domain.Mobile;
+import com.heech.hellcoding.core.member.dto.MemberSearchCondition;
+import com.heech.hellcoding.core.member.service.MemberService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * apiMemberController 통합 테스트
+ */
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class ApiMemberControllerIntegrationTest {
 
     //REQUEST_URL
@@ -34,4 +68,93 @@ public class ApiMemberControllerIntegrationTest {
     public static final String UPDATE_EMAIL = "update_" + EMAIL;
     public static final String UPDATE_BIRTH_DATE = "20001009";
     public static final String UPDATE_PHONE_NUMBER = "01064884296";
+
+    @PersistenceContext EntityManager em;
+
+    @Autowired MockMvc mockMvc;
+
+    @Autowired ObjectMapper objectMapper;
+
+    @Autowired MemberService memberService;
+
+    private Member getMember(String name, String loginId, String password, String email, String birthDate, AuthorCode authorCode, GenderCode genderCode, Mobile mobile, Address address) {
+        Member member = Member.createMemberBuilder()
+                .name(name)
+                .loginId(loginId)
+                .password(password)
+                .email(email)
+                .birthDate(birthDate)
+                .authorCode(authorCode)
+                .genderCode(genderCode)
+                .mobile(mobile)
+                .address(address)
+                .build();
+        return member;
+    }
+
+    @Test
+    @DisplayName(value = "멤버 목록 조회")
+    void findMembersTest() throws Exception {
+        //given
+        Member member = getMember(NAME, LOGIN_ID, PASSWORD, EMAIL, BIRTH_DATE, AUTHOR_CODE, GENDER_CODE, MOBILE, ADDRESS);
+        Member savedMember = memberService.saveMember(member);
+
+        //when
+        ResultActions resoultActions = mockMvc.perform(MockMvcRequestBuilders.get(API_FIND_MEMBER, savedMember.getId())
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        resoultActions.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(HttpStatus.OK.getReasonPhrase()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.memberId").value(member.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.memberName").value(NAME))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.email").value(EMAIL))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.birthDate").value(BIRTH_DATE))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.author").value(AUTHOR_CODE.getCodeName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.gender").value(GENDER_CODE.getCodeName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.phoneNumber").value(MOBILE.fullPhoneNumber()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.address").value(ADDRESS.fullAddress()))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName(value = "멤버 단건 조회")
+    void findMemberTest() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    @DisplayName(value = "멤버 저장")
+    void saveMemberTest() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    @DisplayName(value = "멤버 수정")
+    void updateMemberTest() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    @DisplayName(value = "멤버 삭제")
+    void deleteMemberTest() throws Exception {
+        //given
+
+        //when
+
+        //then
+
+    }
 }
