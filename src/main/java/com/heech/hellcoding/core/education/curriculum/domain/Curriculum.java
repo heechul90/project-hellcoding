4b009a752370eddbc72e7e1fee63033a08b299a3
hclee <hclee@dataeum.kr>
2022-08-01 22:03:56 +0900

    education > curriculum 추가중

--- src/main/java/com/heech/hellcoding/core/education/curriculum/domain/Curriculum.java
+++ src/main/java/com/heech/hellcoding/core/education/curriculum/domain/Curriculum.java
@@ -1,5 +1,6 @@
 package com.heech.hellcoding.core.education.curriculum.domain;
 
+import com.heech.hellcoding.core.education.lesson.domain.Lesson;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,5 +16,14 @@ public class Curriculum {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "education_curriculum_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "education_lesson_id")
+    private Lesson lesson;
+
+    private String dtype;
+
+    private int sortNo;
+    private String CurriculumTitle;
 
 }
