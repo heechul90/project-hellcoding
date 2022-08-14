c172e0b4e3453e683463d07efa7089a0c5c402b1
hclee <hclee@dataeum.kr>
2022-08-14 12:36:55 +0900

    education > live > domain meeting 추가

--- src/main/java/com/heech/hellcoding/core/education/live/domain/Meeting.java
+++ src/main/java/com/heech/hellcoding/core/education/live/domain/Meeting.java
@@ -4,4 +4,17 @@ public class Meeting {
 
     private String serviceId;
     private String contentId;
+    private String meetingId;
+    private String launchUrl;
+    private String hostId;
+    private String identityProvider;
+    private String title;
+    private String password;
+    private Integer maxParticipantCount;
+    private boolean enableJoinAnytime;
+    private boolean enableWaitingRoom;
+    private boolean enablePromptPassword;
+    private boolean enableAccessGrant;
+    private boolean enableEventCallbakc;
+
 }
